b6c213edf76f9e46bc673ce885b51f468be78641
KeniSV <u202018789@upc.edu.pe>
2022-11-17 22:52:56 -0500

    feature: Se creo los Query; primero, relacion simple; segundo, relacion con tablas

--- src/main/java/pe/upc/edu/LeZirconiumSudisTel/ServicesInt/IEmployerService.java
+++ src/main/java/pe/upc/edu/LeZirconiumSudisTel/ServicesInt/IEmployerService.java
@@ -1,6 +1,7 @@
 package pe.upc.edu.LeZirconiumSudisTel.ServicesInt;
 
 import pe.upc.edu.LeZirconiumSudisTel.Entities.Employer;
+import pe.upc.edu.LeZirconiumSudisTel.Entities.RespuestaEmployer;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +12,7 @@ public interface IEmployerService {
     public void delete(int idEmployer);
     List<Employer> search(String first_nameEmployer);
     public Optional<Employer> listarid(int idEmployer);
+    List<Employer> buscarEmail(String correo);
+    List<Employer> buscarDominio();
+    List<RespuestaEmployer> buscarCantidadRoles();
 }
