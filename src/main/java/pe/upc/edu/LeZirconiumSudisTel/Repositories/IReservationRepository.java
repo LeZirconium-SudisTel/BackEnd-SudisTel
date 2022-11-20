527a7d3b20c3dfc067472965c59a683746aafa6d
LoelDev <pmpinzasr@hotmail.com>
2022-11-20 13:28:28 -0500

    feature: se crearon los Querys

--- src/main/java/pe/upc/edu/LeZirconiumSudisTel/Repositories/IReservationRepository.java
+++ src/main/java/pe/upc/edu/LeZirconiumSudisTel/Repositories/IReservationRepository.java
@@ -1,8 +1,13 @@
 package pe.upc.edu.LeZirconiumSudisTel.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pe.upc.edu.LeZirconiumSudisTel.Entities.Reservation;
 
-public interface IReservationRepository extends JpaRepository<Reservation, Integer> {
+import java.util.List;
 
+public interface IReservationRepository extends JpaRepository<Reservation, Integer> {
+    @Query(value = "SELECT cc.id_credit_card, count(r.id) from reservations r inner join credit_card cc on cc.id_credit_card = r.id_credit_card group by cc.id_credit_card", nativeQuery = true)
+    List<String[]> buscarCantidadTarjetas();
 }
+
